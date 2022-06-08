cb200a5e5667aea262346b75a175db21c4c09347
Eugenia Galkina <yavsehmilee@gmail.com>
2022-06-08 23:08:33 +0300

    sum of elements from m to n by RECURSION

---
+++ task01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int result = 0;
+
+void Sum (int n, int m)
+{
+    if (m<=n)
+    {
+        result = result + m;
+        m++;
+        Sum (n,m);
+    }
+}  
+Sum (n,m);
+Console.WriteLine($"Cумма чисел от {m} до {n} = {result}");
