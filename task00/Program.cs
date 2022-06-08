6ee963b3b7062d9d0d1fb4af56a32cc5988afb89
Eugenia Galkina <yavsehmilee@gmail.com>
2022-06-08 23:07:33 +0300

    numbers form m to n by RECURSION

---
+++ task00/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+void Numbers (int n, int m)
+{
+    if (m<=n)
+    {
+        Console.Write(m + " ");
+        m++;
+        Numbers (n,m);
+   
+    }
+}  
+
+Numbers(n,m);
